90d51dfa18542f51c7b365f27b059834a450c0c3
yusuphJr <yusuphnandonde44@gmail.com>
2025-06-20 01:13:39 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -63,4 +63,8 @@ def export_stats():
     return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
