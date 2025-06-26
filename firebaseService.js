8abd6642d85bc8db356f00741115eb5b88badf80
yusuphJr <yusuphnandonde44@gmail.com>
2025-06-26 09:55:40 -0700

    Update firebaseService.js

--- firebaseService.js
+++ firebaseService.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./anonymous-bot-d6875-firebase-adminsdk-fbsvc-8757389a7f.json");
+var serviceAccount = require("./anonymous-bot-d6875-firebase-adminsdk-fbsvc-9cf8d83abf.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
